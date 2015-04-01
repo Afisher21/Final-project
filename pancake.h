206cfa655f856ccef5e067f3035d78d44985da77
Afisher21 <Afisher21@users.noreply.github.com>
2015-04-01 12:12:36 -0500

    Update pancake.h

--- pancake.h
+++ pancake.h
@@ -3,20 +3,20 @@
 #include "Graph.h"
 // Pancake class
 const double Pi=3.14159265359;
-struct Pancake:Ellipse{
-	Pancake(Point poi, int ww, int hh){p=poi;width=ww;height=hh;Ellipse(p,width,height);} // center, min , max from center
-	 
-  //Point button_location() const{return but_point;};
-  int button_width() const {return 2*major();}
-  int button_height() const {return 2*minor();}
-  
-  // Test of githubs ability to version control
-  // pancake 19
-  
-  /*double area_out() const {return area;}
-  private:
-  double area=Pi*width*height;
-  Point but_point=(p.x-height,p.y-width);
-  Point p;
-  int width,height;     */
+struct Pancake : Ellipse{
+	Pancake(Point poi,int width,int height):p(poi),w(width),h(height), Ellipse(p,w,h){
+	elly=new Ellipse(p,w,h);
+	}
+	int button_width() const {return 2*w;}
+	int button_height() const {return 2*h;}
+	Point button_location() const {return Point{p.x-h,p.y-w};}
+	double area() const {return area_ellipse;}
+	void set_fill_color(Color col) { elly->set_fill_color(col);}
+	void set_color(Color col) { elly->set_fill_color(col); }
+	void draw_lines() const{elly->draw_lines();}
+private:
+	Point p;
+	Ellipse* elly;
+	int w,h;
+	double area_ellipse=Pi*w*h;
 };
